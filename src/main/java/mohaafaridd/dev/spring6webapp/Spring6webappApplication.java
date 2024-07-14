7c19e145deb11bc989a29b3400083a85d9f2424a
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-07-14 04:29:55 +0300

    initial commit

---
+++ src/main/java/mohaafaridd/dev/spring6webapp/Spring6webappApplication.java
@@ -0,0 +1,15 @@
+package mohaafaridd.dev.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6webappApplication.class, args);
+	}
+
+
+
+}
