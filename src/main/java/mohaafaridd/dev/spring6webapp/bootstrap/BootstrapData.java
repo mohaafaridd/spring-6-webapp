3249dda88596910fb93e3ac7b393608d5bcf0edb
Mohammed Farid <dev.mohammedfarid@gmail.com>
2024-07-14 05:15:09 +0300

    publisher task

--- src/main/java/mohaafaridd/dev/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/mohaafaridd/dev/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package mohaafaridd.dev.spring6webapp.bootstrap;
 
 import mohaafaridd.dev.spring6webapp.domain.Author;
 import mohaafaridd.dev.spring6webapp.domain.Book;
+import mohaafaridd.dev.spring6webapp.domain.Publisher;
 import mohaafaridd.dev.spring6webapp.repositories.AuthorRepository;
 import mohaafaridd.dev.spring6webapp.repositories.BookRepository;
+import mohaafaridd.dev.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Component;
 public class BootstrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -27,8 +31,16 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("1234");
 
+        Publisher codingBookPublisher = new Publisher();
+        codingBookPublisher.setName("Coding Book Publisher");
+        codingBookPublisher.setCity("NY");
+        codingBookPublisher.setAddress("New York");
+        codingBookPublisher.setState("NY");
+        codingBookPublisher.setZip("123");
+
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
+        Publisher codingBookPublisherSaved = publisherRepository.save(codingBookPublisher);
 
         Author tawfiq = new Author();
         tawfiq.setFirstName("Ahmed");
@@ -38,8 +50,16 @@ public class BootstrapData implements CommandLineRunner {
         icarus.setTitle("Icarus");
         icarus.setIsbn("999");
 
+        Publisher novelPublisher = new Publisher();
+        novelPublisher.setName("Novel Publisher");
+        novelPublisher.setCity("Kafr Abdo");
+        novelPublisher.setAddress("12 Khaled Ali");
+        novelPublisher.setState("Alexandria");
+        novelPublisher.setZip("3333");
+
         Author tawfiqSaved = authorRepository.save(tawfiq);
         Book icarusSaved = bookRepository.save(icarus);
+        Publisher novelPublisherSaved = publisherRepository.save(novelPublisher);
 
         ericSaved.getBooks().add(dddSaved);
         tawfiqSaved.getBooks().add(icarusSaved);
@@ -50,5 +70,6 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap Data");
         System.out.println("Author Count: "+authorRepository.count());
         System.out.println("Book Count: "+bookRepository.count());
+        System.out.println("Publisher Count: "+publisherRepository.count());
     }
 }
---
+++ src/main/java/mohaafaridd/dev/spring6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package mohaafaridd.dev.spring6webapp.repositories;
+
+import mohaafaridd.dev.spring6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
